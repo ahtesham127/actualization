6de3865d9c37382d0e1da80cc3e093a217501d8e
ahtesham127 <ahteshamm947@gmail.com>
2022-07-07 11:13:19 +0500

    updatedd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React from "react";
 import Main from "./components/Main";
+import './animate.css';
 
 function App() {
   return (
--- src/components/OurServices.js
+++ src/components/OurServices.js
@@ -1,12 +1,17 @@
 import React from "react";
+import tablet from "../assets/Tablet.svg";
+import dairy from "../assets/Diary.svg"
 
 const OurServices = () => {
   return (
     <div className="ourServices">
      <div className="content">
-           <h1>Our Services</h1> 
+           <h1  className="animate__animated animate__backInUp">Our Services</h1> 
+           <div className="sections">
+              <img className="animate__animated animate__zoomIn tabletimg" src={tablet} />
+              <img className="animate__animated animate__zoomIn dairyimg" src={dairy} />
+           </div>
      </div>
-      
     </div>
   );
 };

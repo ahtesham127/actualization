60d7b4154108a375ca8bdbcbaaeabbcbe3f2b049
ahtesham127 <ahteshamm947@gmail.com>
2022-07-08 11:06:14 +0500

    updated

--- src/components/OurServices.js
+++ src/components/OurServices.js
@@ -1,32 +1,49 @@
-import React, {useRef, useEffect, useState} from "react";
+import React from "react";
 import tablet from "../assets/Tablet.svg";
 import dairy from "../assets/Diary.svg"
+import mobile from "../assets/Mobile.svg"
 
 const OurServices = () => {
-    const [offset, setOffset] = useState(0);
-    
-    const ref = useRef(null);
-    const addclass=()=>{
-        const sections = ref.current; // corresponding DOM node
-        sections.className = "active";
-     }  
-
-     useEffect(() => {
-        const onScroll = () => setOffset(window.pageYOffset);
-        addclass()
-        window.removeEventListener('scroll', onScroll);
-        window.addEventListener('scroll', onScroll, { passive: true });
-        return () => window.removeEventListener('scroll', onScroll);
-    }, [offset]);
-    
   return (
     <div className="ourServices">
      <div className="content">
-           <h1  className="animate__animated animate__backInUp">Our Services</h1> 
-           <div className="sections" ref={ref}>
-              <img className="animate__animated animate__zoomIn tabletimg" src={tablet} />
-              <img className="animate__animated animate__zoomIn dairyimg" src={dairy} />
-           </div>
+      <section className="blocksection page1">
+        <div className="sections">
+          <div  className="animate__animated animate__backInUp" >
+            <h1 >Our Services</h1> 
+          </div>
+        </div>
+      </section>
+      
+      <section className="blocksection page2">
+        <div className="sections">
+          <img className="animate__animated animate__zoomIn tabletimg" src={tablet} />
+          <img className="animate__animated animate__zoomIn dairyimg" src={dairy} />
+          <div  className="animate__animated animate__zoomIn title1" >
+            <h1 >Our Services</h1> 
+          </div>
+        </div>
+      </section>
+
+      <section className="blocksection page3">
+        <div className="sections">
+          <img className="animate__animated animate__zoomIn tabletimg mobileimg" src={mobile} />
+          <div  className="animate__animated animate__zoomIn title1 smartimg" >
+            <img className="animate__animated animate__zoomIn tabletimg smartimg_Inner" src={tablet} />
+
+          </div>
+        </div>
+      </section>
+
+
+      <section className="blocksection page4">
+        <div className="sections">
+          <img className="animate__animated animate__zoomIn tabletimg" src={tablet} />
+          <img className="animate__animated animate__zoomIn dairyimg" src={dairy} />
+        </div>
+      </section>
+
+
      </div>
     </div>
   );
